52465fcf2d05626f4080d03a01674a5ee7568fe8
Vinicius de Oliveira Santos <vinicius.deoliveira@outlook.com>
2019-06-25 02:38:04 -0300

    Tests for User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_length_of(:name).is_at_least(3) }
+  it { is_expected.to validate_length_of(:name).is_at_most(100) }
+  it { is_expected.to allow_value('').for(:name) }
+  it { is_expected.to have_secure_password }
+
+  describe 'validations' do
+    subject  { create(:user) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  end
+
+  describe 'before validation' do
+    let(:user) { create(:user) }
+
+    it 'convert email to downcase' do
+      expect(user.email).to eq(user.email.downcase)
+    end
+  end
+end
