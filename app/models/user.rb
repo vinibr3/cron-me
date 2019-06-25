8375ac51dd5596e5769f3e376c2a715a2cbc7b2d
Vinicius de Oliveira Santos <vinicius.deoliveira@outlook.com>
2019-06-25 02:38:51 -0300

    Create User model

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false }
+
+  validates :name, length: { within: 3..100 },
+                   format: { with: /\A[^[:cntrl:]\\<>\/&]*\z/ }, # Unicode, permissive
+                   allow_blank: true
+
+  before_validation :convert_email_to_downcase
+
+  private
+
+  def convert_email_to_downcase
+    self.email = self.email.to_s.downcase
+  end
+end
