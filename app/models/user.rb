c320b367fdaba3d6ef8388398c02c90476b105a6
Vinicius de Oliveira Santos <vinicius.deoliveira@outlook.com>
2019-06-25 04:18:15 -0300

    User registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :password_confirmation
   has_secure_password
 
   validates :email, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   scope "/:locale" do
+    resources :users, only: [:new, :create]
+    resources :sessions, only: [:new]
   end
 end
