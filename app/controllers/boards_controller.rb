7d3ed8a5515591f4d1aff2e8b6e840f7e2f64932
vinibr3 <vinicius.deoliveira@outlook.com>
2019-06-25 21:05:38 -0300

    Permit create boards

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,24 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = current_user.boards
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = current_user.boards.new(valid_params)
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def valid_params
+    params.require(:board).permit(:title, :description, :priority, :color)
+  end
+end
