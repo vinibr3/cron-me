94c7fc02b2175e606ea0df4ce1a3d819d2cc2192
Vinicius de Oliveira Santos <vinicius.deoliveira@outlook.com>
2019-06-30 16:37:53 -0300

    Tests for new post controller

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe PostsController, type: :controller do
+  let(:board) { create(:board) }
+  let(:default_locale) { I18n.default_locale }
+  let(:params) { { locale: I18n.default_locale, board_id: board.id } }
+
+
+  describe 'GET new' do
+    before { get :new, params: params }
+
+    it { is_expected.to render_template(:new) }
+    it { is_expected.to route(:get, "/#{default_locale}/boards/#{board.id}/posts/new")
+                        .to(action: :new,
+                            locale: default_locale,
+                            board_id: board.id) }
+  end
+end
