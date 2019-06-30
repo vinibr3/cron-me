aba66791989406d19d0ac462ecde9187cba56e7e
Vinicius de Oliveira Santos <vinicius.deoliveira@outlook.com>
2019-06-30 18:54:12 -0300

    Edit 'post' model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
-  before_action :find_board_to_create_post, only: [:new, :create]
+  before_action :find_board
 
   def new
-    @board = find_board_to_create_post
+    @board = find_board
     @post = @board.posts.new
   end
 
   def create
-    @post = find_board_to_create_post.posts.new(valid_params)
+    @post = find_board.posts.new(valid_params)
     if @post.save
       redirect_to boards_path
     else
@@ -15,13 +15,30 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = @board.posts
+                  .where(id: params[:id])
+                  .first
+  end
+
+  def update
+    @post = @board.posts
+                  .where(id: params[:id])
+                  .first
+    if @post.update_attributes(valid_params)
+      redirect_to boards_path
+    else
+      render :edit
+    end
+  end
+
   private
 
-  def find_board_to_create_post
-    Board.find(params[:board_id])
+  def find_board
+    @board = Board.find(params[:board_id])
   end
 
   def valid_params
-    params.require(:post).permit(:title, :description, :conclusion_deadline)
+    params.require(:post).permit(:title, :description, :conclusion_deadline, :conclusion_date)
   end
 end
