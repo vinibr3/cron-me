351864c20d986ed0ef970dccbeb10e763402d0bb
vinibr3 <vinicius.deoliveira@outlook.com>
2019-07-08 18:02:30 -0300

    Raise create post error message

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,22 @@
+class Api::PostsController < ApplicationController
+  before_action :find_board
+
+  def create
+    post = @board.posts.new(valid_params)
+    head :ok if post.save!
+  rescue StandardError => error
+    error_message = error.message + '\n' + error.backtrace.join('\n')
+    render json: { type: 'error',
+                   message: error_message }, status: 422
+  end
+
+  private
+
+  def valid_params
+    params.require(:post).permit(:title, :description, :conclusion_deadline)
+  end
+
+  def find_board
+    @board = Board.find(params[:board_id])
+  end
+end
